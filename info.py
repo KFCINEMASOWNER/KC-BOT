c527b159b6b8474f2cc6dcd50ecc3acc93f4467d
KFCINEMASOWNER <ujwalnayak25@gmail.com>
2025-01-26 13:34:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -126,9 +126,9 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'adrinolinks.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '41252b5878875a031c9034bd730b5c3ce458b1ae')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/KF_Links/197') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortxlinks.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'ba1103ff3500b60d006da49756ebafff3b8f6550')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/KF_Links/129') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
